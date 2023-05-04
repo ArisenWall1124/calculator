09ddd16a514fd13ce25a542ff86da4263796e549
ArisenWall1124 <69036572+ArisenWall1124@users.noreply.github.com>
2023-05-04 17:12:52 +0200

    Add files via upload

---
+++ Calculator.cpp
@@ -0,0 +1,59 @@
+#include <iostream>
+
+void calc();
+int x = 1;
+std::string yorn;
+bool reset = true;
+
+int main() {
+
+    while (reset == true)
+    {
+        calc();
+    }
+    
+}
+
+void calc() {
+    float a;
+    std::string sym;
+    float b;
+
+    std::cout << "Elso szam: ";
+    std::cin >> a;
+    std::cout << "Muveleti jel (+; -; *; /): ";
+    std::cin >> sym;
+    std::cout << "Masodik szam: ";
+    std::cin >> b;
+    std::cout << "--------------------------------\n";
+    
+    if (sym == "+") 
+    {
+        std::cout << (a + b) << "\n";
+    } else if (sym == "-")
+    {
+        std::cout << (a - b) << "\n";
+    } else if (sym == "*")
+    {
+        std::cout << (a * b) << "\n";
+    } else if (sym == "/")
+    {
+        if (b != 0) {
+            std::cout << (a / b) << "\n";
+        } else {
+            std::cout << "Can't divide by 0!\n";
+        }
+    } else {
+        std::cout << "Invalid operation symbol!\n";
+    }
+    
+    std::cout << "\nContinue? (y/n)\n";
+    std::cin >> yorn;
+    if (yorn == "y") {
+        reset = true;
+    } else
+    {
+        reset = false;
+    }
+    
+}
